64a57a79716834d2116b34aec24aa67cf040705d
gabriellalmeida <almeidagabriel016@gmail.com>
2022-03-25 00:40:19 -0300

    add area de teste

---
+++ src/dio/vto/testes.java
@@ -0,0 +1,13 @@
+package dio.vto;
+
+public class testes {
+             public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        double r;
+
+
+        r = (double) b / a;
+        System.out.printf("%.1f %n", r);
+    }
+}
